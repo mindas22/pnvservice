33945e4f526bb90f57fab0cefca8c932b52c8177
Mindas22 <mindaugas@mintis.net>
2017-08-08 19:32:31 +0100

    initial project version

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+    // $("#loft").on("click", function(e) {
+    //     e.preventDefault(); // in case it is an a element
+    //     var href = e.target.getAttribute("href");
+    //     window.location.href = "https:www.ieskok.lt";
+    // });
